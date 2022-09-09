816b1bc8e86f2ba346d41b58ed24baaef6367da7
Tracy <tracynjeri59@gmail.com>
2022-09-09 09:10:54 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - Entry point
- * Return always 0 (success)
+ * Return: always 0 (success)
  */
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
