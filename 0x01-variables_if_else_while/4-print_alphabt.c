58df8fdb9244a68de4616fdc2d58e3504f1e83d2
Tracy <tracynjeri59@gmail.com>
2022-09-12 12:05:23 -0700

    print 3

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,16 @@
  */
 
 int main(void)
+
 {
 	char ch;
 
-	for (ch  = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 
-	if ((ch != 'q') && (ch != 'e'))
+		if ((ch != 'q') && (ch != 'e'))
 
-	{
-		putchar(ch); }
+		{
+			putchar(ch); }
 	putchar('\n');
-	return (0);
+		return (0);
 }
-
