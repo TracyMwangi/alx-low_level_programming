bf5685e4e910ee0753fa1e43f2c42be8e4b70ce0
Tracy <tracynjeri59@gmail.com>
2022-09-12 09:22:07 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,13 @@
  */
 
 int main(void)
+
 {
 	char ch;
-	for(ch= 'a'; ch<= 'z'; ch++)
+	for(ch = 'a'; ch <= 'z'; ch++)
 {
-	putchar(ch);
-	putchar('\n'); }
-	
+	putchar(ch); }
+
+	putchar('\n');
 	return (0);
 	}
