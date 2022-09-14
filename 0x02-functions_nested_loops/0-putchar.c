a29726967c3d8c21f0d1763ee6fcddaa5de442df
Tracy <tracynjeri59@gmail.com>
2022-09-14 13:26:18 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - entry point
- * putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * return: always 0 (success)
  */
 
-int _putchar(char c)
+int main(void)
 
 {
-		return (write(1, &c, 1));
-
+printf("_putchar\n");
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * prints alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+void print_alphabet(void)
+
+{
+	char ch;
+
+	for (ch = 'a', ch <= 'z', ch++); 
+	
+	{
+		_putchar(ch);
+	}
+	
+	putchar('\n');
+
+}
